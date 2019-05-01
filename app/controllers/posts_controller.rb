1ae4f6211166d14522335cbf3f2c47f72f7d8e61
kreestoff <christopher.l.mcintosh@outlook.com>
2019-05-01 16:04:16 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @user = User.new
   end
 
   def index
@@ -8,10 +10,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+
   end
 
   def create
+
     post = Post.create(post_params)
     redirect_to post
   end
